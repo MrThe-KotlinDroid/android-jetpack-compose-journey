6333ee523ce46a521047f36885c6dbb320987b0b
abrar_hamim <abrarhamim2@gmail.com>
2025-09-29 05:11:12 +0600

    feat: create basic business card layout implementing compose card with elevation and styling

--- 1_MyFirstJetpackApp/app/src/main/java/com/abrar/myfirstjetpackapp/MainActivity.kt
+++ 1_MyFirstJetpackApp/app/src/main/java/com/abrar/myfirstjetpackapp/MainActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.abrar.myfirstjetpackapp.ui.theme.MyFirstJetpackAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +31,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyFirstJetpackAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    CreateBizCard(innerPadding)
                 }
             }
         }
@@ -31,17 +39,31 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CreateBizCard(innerPadding: PaddingValues = PaddingValues(0.dp)) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .padding(innerPadding)
+    ) {
+        Card(
+            modifier = Modifier
+                .width(200.dp)
+                .height(390.dp)
+                .padding(12.dp),
+            shape = RoundedCornerShape(corner = CornerSize(15.dp)),
+            colors = CardDefaults.cardColors(containerColor = Color.White),
+            elevation = CardDefaults.cardElevation(5.dp)
+        ) {
+
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun CreateBizCardPreview() {
     MyFirstJetpackAppTheme {
-        Greeting("Android and me")
+        CreateBizCard()
     }
 }
