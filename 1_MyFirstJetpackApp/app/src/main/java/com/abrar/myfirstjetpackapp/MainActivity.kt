984790ea5bb2b3656c9a29400318d147f84ab733
Abrar Hamim <abrarhamim2@gmail.com>
2025-10-28 18:53:51 +0600

    feat: Implement Compose layouts and alignments
    Learned Alignment on Compose For Row, Column and Box

--- 1_MyFirstJetpackApp/app/src/main/java/com/abrar/myfirstjetpackapp/MainActivity.kt
+++ 1_MyFirstJetpackApp/app/src/main/java/com/abrar/myfirstjetpackapp/MainActivity.kt
@@ -3,67 +3,121 @@ package com.abrar.myfirstjetpackapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CornerSize
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.abrar.myfirstjetpackapp.ui.theme.MyFirstJetpackAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             MyFirstJetpackAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    CreateBizCard(innerPadding)
-                }
+                //MyLayouts("Android")
+                MyAlignments()
             }
         }
     }
 }
 
 @Composable
-fun CreateBizCard(innerPadding: PaddingValues = PaddingValues(0.dp)) {
-    Surface(
-        modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight()
-            .padding(innerPadding)
-    ) {
-        Card(
-            modifier = Modifier
-                .width(200.dp)
-                .height(390.dp)
-                .padding(12.dp),
-            shape = RoundedCornerShape(corner = CornerSize(15.dp)),
-            colors = CardDefaults.cardColors(containerColor = Color.White),
-            elevation = CardDefaults.cardElevation(5.dp)
-        ) {
+fun MyAlignments() {
 
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+            //contentAlignment = Alignment.Center) for Box layout
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text(
+            text = "Hello",
+            color = Color.White,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            fontStyle = FontStyle.Italic,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .background(Color.Red)
+                .width(100.dp)
+        )
+        Text(
+            text = "Android",
+            color = Color.White,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            fontStyle = FontStyle.Italic,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .background(Color.Red)
+                .width(100.dp)
+        )
+        Text(
+            text = "Kotlin",
+            color = Color.White,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            fontStyle = FontStyle.Italic,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .background(Color.Red)
+                .width(100.dp)
+        )
+    }
+
+
+}
+
+
+@Composable
+fun MyLayouts(name: String) {
+    Column {
+        Box(
+            modifier = Modifier
+                .background(Color.Green)
+                .height(200.dp)
+                .width(300.dp)
+        ) {
+            Row {
+                Box(
+                    modifier = Modifier
+                        .background(Color.Blue)
+                        .size(50.dp)
+                )
+                Column {
+                    Text(text = "Hello ")
+                    Text(text = "Android!")
+                }
+            }
         }
     }
 }
 
+
 @Preview(showBackground = true)
 @Composable
-fun CreateBizCardPreview() {
+fun DefaultPreview() {
     MyFirstJetpackAppTheme {
-        CreateBizCard()
+        //MyLayouts("Android")
+        MyAlignments()
     }
 }
