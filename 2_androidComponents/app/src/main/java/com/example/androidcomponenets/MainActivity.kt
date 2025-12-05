ff9ad721cb781883e508456f6f52f6a90ea0bfa3
abrarhamim <abrarhamim2@gmail.com>
2025-12-05 16:24:31 +0600

    feat: Add initial project configuration files and update dependencies

--- 2_androidComponents/app/src/main/java/com/example/androidcomponenets/MainActivity.kt
+++ 2_androidComponents/app/src/main/java/com/example/androidcomponenets/MainActivity.kt
@@ -17,13 +17,12 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -32,7 +31,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -45,12 +43,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             AndroidComponenetsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Surface(
-                        color = MaterialTheme.colorScheme.background,
+                    Components(
                         modifier = Modifier.padding(innerPadding)
-                    ) {
-                        Components()
-                    }
+                    )
                 }
             }
         }
@@ -58,18 +53,10 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Components() {
+fun Components(modifier: Modifier = Modifier) {
 
-    val myButtonBackgroundColor = remember {
-        mutableStateOf(Color.Red)
-    }
-
-    val myButtonText = remember {
-        mutableStateOf("Do Your Magic")
-    }
-
-    val myButtonBorderColor = remember {
-        mutableStateOf(Color.Black)
+    val myImage = remember {
+        mutableIntStateOf(R.drawable.first_image)
     }
 
     val myButtonStatus = remember {
@@ -81,15 +68,23 @@ fun Components() {
     }
 
     val userInput = remember {
-        mutableStateOf("Result:")
+        mutableStateOf("U entered: ")
     }
 
-    val myImage = remember {
-        mutableStateOf(R.drawable.first_image)
+    val myText = remember {
+        mutableStateOf("Hello World")
+    }
+
+    val myButtonText = remember {
+        mutableStateOf("Do Your Magic")
+    }
+
+    val myButtonBackgroundColor = remember {
+        mutableStateOf(Color.Red)
     }
 
     Column(
-        modifier = Modifier
+        modifier = modifier
             .fillMaxSize()
             .background(Color.White),
         verticalArrangement = Arrangement.Center,
@@ -97,95 +92,88 @@ fun Components() {
     ) {
 
         Image(
-            painter = painterResource(myImage.value),
+            painter = painterResource(myImage.intValue),
             "First Image",
-            modifier = Modifier.size(300.dp),
+            Modifier.size(300.dp),
             contentScale = ContentScale.Fit,
-            alignment = Alignment.Center
         )
 
-        Spacer(modifier = Modifier.size(10.dp))
+//        if (myButtonStatus.value) {
+//        }
+        Text(
+            text = myText.value,
+            color = Color.White,
+            modifier = Modifier
+                .background(Color.DarkGray)
+                .padding(20.dp)
+        )
 
-        if (myButtonStatus.value) {
-            Text(
-                text = "Hello Compose",
-                modifier = Modifier
-                    .background(Color.DarkGray)
-                    .padding(20.dp),
-                color = Color.White
-            )
-        }
-
-        Spacer(modifier = Modifier.size(30.dp))
-
+        Spacer(Modifier.size(30.dp))
         Button(
             onClick = {
                 if (myButtonStatus.value) {
-                    myButtonBackgroundColor.value = Color.Black
-                    myButtonText.value = "Compose is Fun"
-                    myButtonBorderColor.value = Color.Yellow
-                    myImage.value = R.drawable.second_image
-
+                    myImage.intValue = R.drawable.second_image
+                    myText.value = "Hello Compose!"
+                    myButtonText.value = "Compose is fun!"
+                    myButtonBackgroundColor.value = Color.Cyan
                     myButtonStatus.value = false
-                } else {
-                    myButtonBackgroundColor.value = Color.Red
+                }
+                else{
+                    myImage.intValue = R.drawable.first_image
+                    myText.value = "Hello World!"
                     myButtonText.value = "Do Your Magic"
-                    myButtonBorderColor.value = Color.Black
-                    myImage.value = R.drawable.first_image
-
+                    myButtonBackgroundColor.value = Color.Red
                     myButtonStatus.value = true
                 }
+
                 userInput.value = valueOnTextField.value
                 valueOnTextField.value = ""
             },
-
-
             modifier = Modifier.size(250.dp, 60.dp),
             colors = ButtonDefaults.buttonColors(containerColor = myButtonBackgroundColor.value),
             shape = RoundedCornerShape(10.dp),
-            border = BorderStroke(3.dp, myButtonBorderColor.value)
+            border = BorderStroke(3.dp, Color.Black)
         ) {
             Text(
                 text = myButtonText.value,
                 color = Color.White,
-                fontSize = 20.sp,
-                textAlign = TextAlign.Center
+                fontSize = 20.sp
             )
         }
 
-        Spacer(modifier = Modifier.size(20.dp))
+        Spacer(Modifier.size(30.dp))
 
         TextField(
             value = valueOnTextField.value,
             onValueChange = {
                 valueOnTextField.value = it
             },
-            label = { Text(text = "Enter your name") },
-            modifier = Modifier.width(300.dp),
+            label = { Text(text = "Enter Your Name") },
+            modifier = Modifier
+                .width(300.dp),
             colors = TextFieldDefaults.colors(
-                focusedTextColor = Color.White,
+                focusedTextColor = Color.Black,
                 unfocusedTextColor = Color.White,
-                focusedContainerColor = Color.Blue,
+                focusedContainerColor = Color.White,
+                unfocusedContainerColor = Color.Gray,
                 focusedLabelColor = Color.Yellow,
                 unfocusedLabelColor = Color.White,
                 unfocusedIndicatorColor = Color.Red,
-                focusedIndicatorColor = Color.Green
+                focusedIndicatorColor = Color.White
             ),
             textStyle = TextStyle.Default.copy(fontSize = 20.sp),
-            maxLines = 4,
-            //keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-            //visualTransformation = PasswordVisualTransformation()
+            maxLines = 1
         )
 
-        Spacer(modifier = Modifier.size(20.dp))
+        Spacer(Modifier.size(20.dp))
 
         Text(
             fontSize = 24.sp,
             text = userInput.value,
             modifier = Modifier
-                .background(Color.Gray)
+                .background(Color.LightGray)
                 .padding(10.dp),
-            color = Color.White
+            color = Color.Black
         )
     }
 }
